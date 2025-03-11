15d6e03ee26054229c5761df777d7af6ba8cb675
Tobias Bak <202206068@post.au.dk>
2025-03-11 14:26:22 +0100

    svelte+tailwind added

---
+++ SpotifyCoPlaylist/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1 class="font-[helvetica] text-6xl">
+    ChatDPT's store bachelorprojekt 
+</h1>
+
+
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
---
+++ SpotifyCoPlaylist/src/types.d.ts
@@ -0,0 +1,95 @@
+export interface UserProfile {
+    country: string;
+    display_name: string;
+    email: string;
+    explicit_content: {
+        filter_enabled: boolean,
+        filter_locked: boolean
+    },
+    external_urls: { spotify: string; };
+    followers: { href: string; total: number; };
+    href: string;
+    id: string;
+    images: Image[];
+    product: string;
+    type: string;
+    uri: string;
+}
+
+export interface Image {
+    url: string;
+    height: number;
+    width: number;
+}
+
+// spotify.d.ts
+
+export interface SpotifyTopTracksResponse {
+    items: SpotifyTrack[];
+    total: number;
+    limit: number;
+    offset: number;
+    href: string;
+    previous: string | null;
+    next: string | null;
+  }
+  
+  export interface SpotifyTrack {
+    album: SpotifyAlbum;
+    artists: SpotifyArtist[];
+    available_markets: string[];
+    disc_number: number;
+    duration_ms: number;
+    explicit: boolean;
+    external_ids: {
+      isrc: string;
+    };
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    is_local: boolean;
+    name: string;
+    popularity: number;
+    preview_url: string | null;
+    track_number: number;
+    type: string;
+    uri: string;
+  }
+  
+  export interface SpotifyAlbum {
+    album_type: string;
+    artists: SpotifyArtist[];
+    available_markets: string[];
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    images: SpotifyImage[];
+    name: string;
+    release_date: string;
+    release_date_precision: string;
+    total_tracks: number;
+    type: string;
+    uri: string;
+  }
+  
+  export interface SpotifyArtist {
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    name: string;
+    type: string;
+    uri: string;
+  }
+  
+  export interface SpotifyImage {
+    height: number;
+    url: string;
+    width: number;
+  }
+  
